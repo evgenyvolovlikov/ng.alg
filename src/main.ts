f72edff284b566b36d6951e8d5fe0f1fda599a6b
Евгений Воловликов <evgenyvolovlikov@gmail.com>
2026-05-26 20:14:49 +0300

    update on refactoring

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from '@app/app';
-import { appConfig } from '@app/providers/app.config';
+import { AppComponent } from './app/app';
+import { appConfig } from './app/providers/app.config';
 
 bootstrapApplication(AppComponent, appConfig).catch((e) => console.warn(e));
